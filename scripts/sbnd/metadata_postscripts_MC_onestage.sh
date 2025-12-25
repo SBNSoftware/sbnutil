933cbdfbed80c789e1be21b47b09dcd33cd3c3cc
sbndpro <sbnd@fnal.gov>
2025-12-25 02:25:56 -0600

    Configs and scripts for one-stage MC workflow. Includes the custom AR25 workflow

--- scripts/sbnd/metadata_postscripts_MC_onestage.sh
+++ scripts/sbnd/metadata_postscripts_MC_onestage.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-# Copied from Vito's area, /exp/icarus/app/users/vito/sam/declare_file.sh
-# Adapted for use on postscript stage for UNIFIED/ONESTAGE pipelines
-# This version handles naming scheme: data_filtered_decoded_reco1.root, etc.
+# Adapted from metadata_postscripts_onestage.sh (DATA) for use on MC
 # Essentially, build the metadata for the file manually while ignoring artroot.
 # Most of this stuff is already exported as env variables, from the prescripts stage.
 
 >&2 echo -e "\n\nSTARTING $0...\n\n"
 
-# Retire any declared files before moving on.
-reco1File='' # Artroot reco1 files
-larcvFile='' # LArCV files
-histFile=''  # Calib ntuples
-cafFile=''   # CAF trees
-flatFile=''  # FlatCAF trees
+initialFile=$(basename ${PARENT_FILE_SAM}) # Initial fcl (parent) file
+detsimFile=''  # Detsim files
+reco1File=''   # Artroot reco1 files
+larcvFile=''   # LarCV files
+histFile=''    # Calib ntuples
+cafFile=''     # CAF trees
+flatFile=''    # FlatCAF trees
 
 >&2 echo -e "\n\nUsing fcl list $1...\n\n"
 
-# Find the files.
+detsimFile=$(find ./ -iname '*.root' | grep 'detsim' | grep -v 'larcv' | grep -v 'reco1' | grep -v 'hist' | grep -v 'reco2' | grep -v 'caf')
 reco1File=$(find ./ -iname '*.root' | grep 'reco1' | grep -v 'larcv' | grep -v 'hist' | grep -v 'reco2' | grep -v 'caf')
 larcvFile=$(find ./ -iname 'larcv*.root' | grep -v 'reco2')
 histFile=$(find ./ -iname 'hist*.root' | grep 'reco2' | head -n 1)
-cafFile=$(find ./ -iname '*.caf.root' | grep -v 'flat')
+cafFile=$(find ./ -iname '*caf.root' | grep -v 'flat') # looks like it was literally 'caf.root'..
 flatFile=$(find ./ -iname '*.caf.root' | grep 'flat')
 
 echo -e "Here is the contents of the dir BEFORE renaming..."
 ls -ltrh
 
+echo -e "detsimFile: ${detsimFile}"
 echo -e "reco1File: ${reco1File}"
 echo -e "larcvFile: ${larcvFile}"
 echo -e "histFile: ${histFile}"
 echo -e "cafFile: ${cafFile}"
 echo -e "flatFile: ${flatFile}"
 
-# We will set the reco1 artroot file as the parent for both stages.
-samweb -e ${SAM_EXPERIMENT} get-metadata --json $(basename ${PARENT_FILE_SAM}) > old_par_md.json
-par_name=$(cat old_par_md.json | jq -r .'file_name')
-par_name=${par_name/.root/}
+# Initial fcl is the parent
+>&2 echo -e "\n\nFrom initial fcl file ${initialFile} I get metadata..."
+samweb -e ${SAM_EXPERIMENT} get-metadata --json ${initialFile} > old_par_md.json
+par_name=${initialFile/.root/}
 
-# Get the filenames and rename them to something sensible
-# No need to rename the reco1 stage, I handled that in the cfg.
-mv ${histFile} hist_reco2_${par_name}.root
-histFile=hist_reco2_${par_name}.root
-mv ${cafFile} reco2_${par_name}.caf.root
-cafFile=reco2_${par_name}.caf.root
-mv ${flatFile} reco2_${par_name}.flat.caf.root
-flatFile=reco2_${par_name}.flat.caf.root
-
-# Get the runs 
-subRun=$(grep messages.log -e 'subRun' | head -n 1 | awk -F "subRun: " '{print $2}' | awk -F " " '{print $1}')
-runString=$(cat old_par_md.json | sed 's/commissioning/physics/g' | jq .runs[0])
+# Get the first and last event as well as the run and subrun
+# There could be a more elegant way to do this. Or, we could invoke the wrath of Bash.
+# Scour the message logs, find any lines with timesamps and run/subrun/event, remove away any code,
+# these are in tokens 'run: X subRun: Y event: Z', extract uniques, and sort for first/last based on Z
+source_log=messages.log
+first_token=$(cat ${source_log} | grep -e 'run:' | grep -e 'event:' | grep -v '.cc' | awk -F " run: " '{print $2}' | sed -e 's/^ //' | sort -u | grep -e 'event' | sort -k5,5n | head -n 1)
+last_token=$(cat ${source_log} | grep -e 'run:' | grep -e 'event:' | grep -v '.cc' | awk -F " run: " '{print $2}' | sed -e 's/^ //' | sort -u | grep -e 'event' | sort -k5,5nr | head -n 1)
+run_no=$(echo ${first_token} | awk -F " " '{print $1}')
+subrun_no=$(echo ${first_token} | awk -F " " '{print $3}')
+first_event=$(echo ${first_token} | awk -F " " '{print $5}')
+last_event=$(echo ${last_token} | awk -F " " '{print $5}')
 
 # We'll pass the fcl-file-list here, under fcl.name
 fcl_list=$1
 
 # Rename files uniquely, based on their own md5sum.
 # This ensures that we have unique file names
+md5_detsim=$(md5sum ${detsimFile} | awk -F " " '{print $1}')
 md5_reco1=$(md5sum ${reco1File} | awk -F " " '{print $1}')
 md5_larcv=$(md5sum ${larcvFile} | awk -F " " '{print $1}')
 md5_hist=$(md5sum ${histFile} | awk -F " " '{print $1}')
@@ -63,6 +63,8 @@ md5_caf=$(md5sum ${cafFile} | awk -F " " '{print $1}')
 md5_flat=$(md5sum ${flatFile} | awk -F " " '{print $1}')
 
 # To make things a bit more unique, I will pipe this to a file, append some random numbers, and md5sum
+echo $md5_detsim > hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log
+md5_detsim=$(md5sum ${detsimFile} | awk -F " " '{print $1}')
 echo $md5_reco1 > hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log
 md5_reco1=$(md5sum ${reco1File} | awk -F " " '{print $1}')
 echo $md5_larcv > hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log
@@ -75,6 +77,8 @@ echo $md5_flat > hmd.log ; echo $RANDOM >> hmd.log ; echo $RANDOM >> hmd.log ; e
 md5_flat=$(md5sum ${flatFile} | awk -F " " '{print $1}')
 
 # In case there is a hash in the filenames, remove it
+old_detsim_hash=$(echo ${detsimFile} | sed 's/.*run\([^.]*\)\.root/\1/' | sed -E 's/.*-([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/\1/')
+excised_detsimFile=${detsimFile%-${old_detsim_hash}*}
 old_reco1_hash=$(echo ${reco1File} | sed 's/.*run\([^.]*\)\.root/\1/' | sed -E 's/.*-([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/\1/')
 excised_reco1File=${reco1File%-${old_reco1_hash}*}
 old_larcv_hash=$(echo ${larcvFile} | sed 's/.*run\([^.]*\)\.root/\1/' | sed -E 's/.*-([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/\1/')
@@ -86,12 +90,14 @@ excised_cafFile=${cafFile%-${old_caf_hash}*}
 old_flat_hash=$(echo ${flatFile} | sed 's/.*run\([^.]*\)\.root/\1/' | sed -E 's/.*-([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/\1/')
 excised_flatFile=${flatFile%-${old_flat_hash}*}
 
+used_detsim_hash=${md5_detsim:0:8}-${md5_detsim:8:4}-${md5_detsim:12:4}-${md5_detsim:16:4}-${md5_detsim:20:12}
 used_reco1_hash=${md5_reco1:0:8}-${md5_reco1:8:4}-${md5_reco1:12:4}-${md5_reco1:16:4}-${md5_reco1:20:12}
 used_larcv_hash=${md5_larcv:0:8}-${md5_larcv:8:4}-${md5_larcv:12:4}-${md5_larcv:16:4}-${md5_larcv:20:12}
 used_hist_hash=${md5_hist:0:8}-${md5_hist:8:4}-${md5_hist:12:4}-${md5_hist:16:4}-${md5_hist:20:12}
 used_caf_hash=${md5_caf:0:8}-${md5_caf:8:4}-${md5_caf:12:4}-${md5_caf:16:4}-${md5_caf:20:12}
 used_flat_hash=${md5_flat:0:8}-${md5_flat:8:4}-${md5_flat:12:4}-${md5_flat:16:4}-${md5_flat:20:12}
 
+used_detsim_prebit=${used_detsim_hash:0:2}
 used_reco1_prebit=${used_reco1_hash:0:2}
 used_larcv_prebit=${used_larcv_hash:0:2}
 used_hist_prebit=${used_hist_hash:0:2}
@@ -99,18 +105,21 @@ used_caf_prebit=${used_caf_hash:0:2}
 used_flat_prebit=${used_flat_hash:0:2}
 
 # Definitive names for the outputs
+newDetsimFile=${excised_detsimFile%.root}-${used_detsim_hash}.root
 newReco1File=${excised_reco1File%.root}-${used_reco1_hash}.root
 newLarcvFile=${excised_larcvFile%.root}-${used_larcv_hash}.root
 newHistFile=${excised_histFile%.root}-${used_hist_hash}.root
 newCafFile=${excised_cafFile%.root}-${used_caf_hash}.caf.root
 newFlatFile=${excised_flatFile%.root}-${used_flat_hash}.flat.caf.root
 
+mv ${detsimFile} ${newDetsimFile}
 mv ${reco1File} ${newReco1File}
 mv ${larcvFile} ${newLarcvFile}
 mv ${histFile} ${newHistFile}
 mv ${cafFile} ${newCafFile}
 mv ${flatFile} ${newFlatFile}
 
+echo -e "Definitive detsim name: ${newDetsimFile}"
 echo -e "Definitive reco1 name: ${newReco1File}"
 echo -e "Definitive larcv name: ${newLarcvFile}"
 echo -e "Definitive hist name: ${newHistFile}"
@@ -118,19 +127,21 @@ echo -e "Definitive caf name: ${newCafFile}"
 echo -e "Definitive flat name: ${newFlatFile}"
 
 # Construct json files.
+DETSIM_MD=md_detsim.json
 RECO1_MD=md_reco1.json
 LARCV_MD=md_larcv.json
 HIST_MD=md_hist.json
 CAF_MD=md_caf.json
 FLAT_MD=md_flat.json
 
-touch ${RECO1_MD}; touch ${LARCV_MD}; touch ${HIST_MD}; touch ${CAF_MD}; touch ${FLAT_MD}
+touch ${DETSIM_MD}; touch ${RECO1_MD}; touch ${LARCV_MD}; touch ${HIST_MD}; touch ${CAF_MD}; touch ${FLAT_MD}
 
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
+for mdFile in ${DETSIM_MD} ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e "{" >> ${mdFile}
 done
 
 # file names
+echo -e " \"file_name\": \"$(basename ${newDetsimFile})\"," >> ${DETSIM_MD}
 echo -e " \"file_name\": \"$(basename ${newReco1File})\"," >> ${RECO1_MD}
 echo -e " \"file_name\": \"$(basename ${newLarcvFile})\"," >> ${LARCV_MD}
 echo -e " \"file_name\": \"$(basename ${newHistFile})\"," >> ${HIST_MD}
@@ -138,6 +149,7 @@ echo -e " \"file_name\": \"$(basename ${newCafFile})\"," >> ${CAF_MD}
 echo -e " \"file_name\": \"$(basename ${newFlatFile})\"," >> ${FLAT_MD}
 
 # file sizes
+echo -e " \"file_size\": $(stat -c %s ${newDetsimFile} | awk -F " " '{print $1}')," >> ${DETSIM_MD}
 echo -e " \"file_size\": $(stat -c %s ${newReco1File} | awk -F " " '{print $1}')," >> ${RECO1_MD}
 echo -e " \"file_size\": $(stat -c %s ${newLarcvFile} | awk -F " " '{print $1}')," >> ${LARCV_MD}
 echo -e " \"file_size\": $(stat -c %s ${newHistFile} | awk -F " " '{print $1}')," >> ${HIST_MD}
@@ -145,18 +157,15 @@ echo -e " \"file_size\": $(stat -c %s ${newCafFile} | awk -F " " '{print $1}'),"
 echo -e " \"file_size\": $(stat -c %s ${newFlatFile} | awk -F " " '{print $1}')," >> ${FLAT_MD}
 
 # Specific data tiers for outputs
+echo -e " \"data_tier\": \"detector-simulated\"," >> ${DETSIM_MD}
 echo -e " \"data_tier\": \"reconstructed\"," >> ${RECO1_MD}
 echo -e " \"data_tier\": \"larcv\"," >> ${LARCV_MD}
 echo -e " \"data_tier\": \"root-tuple\"," >> ${HIST_MD}
 echo -e " \"data_tier\": \"caf\"," >> ${CAF_MD}
 echo -e " \"data_tier\": \"flat_caf\"," >> ${FLAT_MD}
 
-# data stream is common
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
-    echo -e  " \"data_stream\": \"${MT_SBND_STREAM_NAME}\"," >> ${mdFile}
-done
-
 # file formats
+echo -e " \"file_format\": \"artroot\"," >> ${DETSIM_MD}
 echo -e " \"file_format\": \"artroot\"," >> ${RECO1_MD}
 echo -e " \"file_format\": \"larcv\"," >> ${LARCV_MD}
 echo -e " \"file_format\": \"root\"," >> ${HIST_MD}
@@ -164,26 +173,29 @@ echo -e " \"file_format\": \"caf\"," >> ${CAF_MD}
 echo -e " \"file_format\": \"flat_caf\"," >> ${FLAT_MD}
 
 # Checksums
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
+for mdFile in ${DETSIM_MD} ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e " \"checksum\": [" >> ${mdFile}
 done
+echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newDetsimFile} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${DETSIM_MD}
 echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newReco1File} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${RECO1_MD}
 echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newLarcvFile} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${LARCV_MD}
 echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newHistFile} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${HIST_MD}
 echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newCafFile} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${CAF_MD}
 echo -e "$(samweb -e ${SAM_EXPERIMENT} file-checksum --type=enstore,adler32,md5 ${newFlatFile} | tr -d '[' | tr -d ']' | tr ' ' '\n')" >> ${FLAT_MD}
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
+for mdFile in ${DETSIM_MD} ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e " ]," >> ${mdFile}
 done
 
 # Dataset tags
-export DTAG_PREAMBLE=${MT_PRODUCTIONTYPE}_${MT_PRODUCTIONLABEL}_${MT_PRODUCTIONNAME}_${SBNDCODE_VERSION}
+export DTAG_PREAMBLE=${MT_PRODUCTIONTYPE}_${MT_PRODUCTIONLABEL}_${MT_FCLNAME}_${MT_SAMPLE}_${SBNDCODE_VERSION}
+export DETSIM_DTAG=${DTAG_PREAMBLE}_detsim_sbnd
 export RECO1_DTAG=${DTAG_PREAMBLE}_reco1_sbnd
 export LARCV_DTAG=${DTAG_PREAMBLE}_larcvreco1_sbnd
 export HIST_DTAG=${DTAG_PREAMBLE}_histreco2_sbnd
 export CAF_DTAG=${DTAG_PREAMBLE}_caf_sbnd
 export FLAT_DTAG=${DTAG_PREAMBLE}_flatcaf_sbnd
 
+echo -e " \"Dataset.Tag\": \"${DETSIM_DTAG}\"," >> ${DETSIM_MD}
 echo -e " \"Dataset.Tag\": \"${RECO1_DTAG}\"," >> ${RECO1_MD}
 echo -e " \"Dataset.Tag\": \"${LARCV_DTAG}\"," >> ${LARCV_MD}
 echo -e " \"Dataset.Tag\": \"${HIST_DTAG}\"," >> ${HIST_MD}
@@ -191,9 +203,9 @@ echo -e " \"Dataset.Tag\": \"${CAF_DTAG}\"," >> ${CAF_MD}
 echo -e " \"Dataset.Tag\": \"${FLAT_DTAG}\"," >> ${FLAT_MD}
 
 # Common tags
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
+for mdFile in ${DETSIM_MD} ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e " \"process_id\": ${SAM_CONSUMER_ID}," >> ${mdFile}
-    echo -e " \"file_type\": \"data\"," >> ${mdFile}
+    echo -e " \"file_type\": \"mc\"," >> ${mdFile}
     echo -e " \"group\": \"sbnd\"," >> ${mdFile}
     echo -e " \"application\": {\n\t\"family\": \"art\",\n\t\"name\": \"sbndcode\",\n\t\"version\": \"${SBNDCODE_VERSION}\"\n }," >> ${mdFile}
     echo -e " \"art.file_format_era\": \"ART_2011a\"," >> ${mdFile}
@@ -201,39 +213,43 @@ for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e " \"art.run_type\": \"physics\"," >> ${mdFile}
 done
 # Insert process names
+echo -e " \"art.process_name\": \"DetSim\"," >> ${DETSIM_MD}
 echo -e " \"art.process_name\": \"Reco1\"," >> ${RECO1_MD}
 echo -e " \"art.process_name\": \"Reco1\"," >> ${LARCV_MD}
 echo -e " \"art.process_name\": \"Reco2\"," >> ${HIST_MD}
 echo -e " \"art.process_name\": \"caf\"," >> ${CAF_MD}
 echo -e " \"art.process_name\": \"caf\"," >> ${FLAT_MD}
+
 # Back to common tags
-for mdFile in ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
+for mdFile in ${DETSIM_MD} ${RECO1_MD} ${LARCV_MD} ${HIST_MD} ${CAF_MD} ${FLAT_MD} ; do
     echo -e " \"fcl.name\": \"${fcl_list}\"," >> ${mdFile}
     echo -e " \"production.name\": \"${MT_PRODUCTIONNAME}\"," >> ${mdFile}
     echo -e " \"production.type\": \"${MT_PRODUCTIONTYPE}\"," >> ${mdFile}
-    echo -e " \"configuration.name\": \"${MT_CONFIGURATION}\"," >> ${mdFile}
-    echo -e " \"sbn_dm.beam_type\": \"${MT_BEAMTYPE}\"," >> ${mdFile}
-    echo -e " \"sbn_dm.detector\": \"${MT_DETECTOR}\"," >> ${mdFile}
-    echo -e " \"sbn_dm.event_count\": ${MT_EVENTCOUNT}," >> ${mdFile}
-    echo -e " \"sbnd.random\": \"${MT_RANDOM}\"," >> ${mdFile}
-    if [[ ! -z ${MT_EVENTSTRING} ]] ; then
-	echo -e " \"sbnd.random_run\": \"${MT_RANDOMRUN}\"," >> ${mdFile}
-	echo -e " \"sbnd.event_number_list\": \"${MT_EVENTSTRING}\"," >> ${mdFile}
-    else
-	echo -e " \"sbnd.random_run\": \"${MT_RANDOMRUN}\"," >> ${mdFile}
-    fi
+    echo -e " \"mc.generated_event_count\": ${MT_EVENTS_PER_JOB}," >> ${mdFile}
+    echo -e " \"data_stream\": \"out1\"," >> ${mdFile}
     echo -e " \"sbnd_project.name\": \"${MT_PROJECTNAME}\"," >> ${mdFile}
     echo -e " \"sbnd_project.software\": \"${MT_PROJECTSOFTWARE}\"," >> ${mdFile}
     echo -e " \"sbnd_project.stage\": \"${MT_PROJECTSTAGE}\"," >> ${mdFile}
     echo -e " \"sbnd_project.version\": \"${SBNDCODE_VERSION}\"," >> ${mdFile}
+    echo -e " \"mc.genie_tune\": \"${MT_GENIE_TUNE}\"," >> ${mdFile}
+    echo -e " \"mc.genie_tag\": \"${MT_GENIE_TAG}\"," >> ${mdFile}
     echo -e " \"runs\": [" >> ${mdFile}
-    echo -e " ${runString}" >> ${mdFile}
+    echo -e "   [" >> ${mdFile}
+    echo -e "     ${run_no}," >> ${mdFile}
+    echo -e "     ${subrun_no}," >> ${mdFile}
+    echo -e "     \"physics\"" >> ${mdFile}
+    echo -e "   ]" >> ${mdFile}
     echo -e " ]," >> ${mdFile}
+    echo -e " \"first_event\": ${first_event}," >> ${mdFile}
+    echo -e " \"last_event\": ${last_event}," >> ${mdFile}
     echo -e " \"parents\": [" >> ${mdFile}
     echo -e "\t{ \"file_name\": \"${PARENT_FILE_SAM}\" }" >> ${mdFile}
     echo -e "  ]" >> ${mdFile}
     echo -e " }" >> ${mdFile}
 done
+
+echo -e "Here is the detsim json..."
+cat ${DETSIM_MD} | sed '/^$/d'
 
 echo -e "Here is the reco1 json..."
 cat ${RECO1_MD} | sed '/^$/d'
@@ -254,6 +270,9 @@ echo "Here are the contents of my workdir..."
 ls -ltrh
 
 # Retire any declared files before moving on.
+checkMeta=$(samweb -e ${SAM_EXPERIMENT} get-metadata $(basename $newDetsimFile))
+if [[ $? == 0 ]] ; then
+    samweb -e ${SAM_EXPERIMENT} retire-file $(basename $newDetsimFile) ; fi
 checkMeta=$(samweb -e ${SAM_EXPERIMENT} get-metadata $(basename $newReco1File))
 if [[ $? == 0 ]] ; then
     samweb -e ${SAM_EXPERIMENT} retire-file $(basename $newReco1File) ; fi
@@ -272,6 +291,7 @@ if [[ $? == 0 ]] ; then
 
 # now validate the metadata
 echo "Validating metadata...."
+samweb -e ${SAM_EXPERIMENT} validate-metadata ${DETSIM_MD}
 samweb -e ${SAM_EXPERIMENT} validate-metadata ${RECO1_MD}
 samweb -e ${SAM_EXPERIMENT} validate-metadata ${LARCV_MD}
 samweb -e ${SAM_EXPERIMENT} validate-metadata ${HIST_MD}
@@ -280,12 +300,29 @@ samweb -e ${SAM_EXPERIMENT} validate-metadata ${FLAT_MD}
 
 # and finally, declare these!
 # Set output directories based on stage
+FULL_DETSIM_OUTDIR=${IFDH_OUTPUT_DIR}/detsim/${MT_SBND_STREAM_NAME}/${used_detsim_prebit}/
 FULL_RECO1_OUTDIR=${IFDH_OUTPUT_DIR}/reco1/${MT_SBND_STREAM_NAME}/${used_reco1_prebit}/
 FULL_LARCV_OUTDIR=${IFDH_OUTPUT_DIR}/larcv/${MT_SBND_STREAM_NAME}/${used_larcv_prebit}/
 FULL_HIST_OUTDIR=${IFDH_OUTPUT_DIR}/calib/${MT_SBND_STREAM_NAME}/${used_hist_prebit}/
 FULL_CAF_OUTDIR=${IFDH_OUTPUT_DIR}/caf/${MT_SBND_STREAM_NAME}/${used_caf_prebit}/
 FULL_FLAT_OUTDIR=${IFDH_OUTPUT_DIR}/flatcaf/${MT_SBND_STREAM_NAME}/${used_flat_prebit}/
 
+# Override for detsim: this will put our disk space in trouble
+COPY_BACK_DETSIM=0
+if [[ ${COPY_BACK_DETSIM} == 1 ]] ; then
+    ###### DETSIM DECLARATION ######
+    if samweb -e ${SAM_EXPERIMENT} validate-metadata ${DETSIM_MD} ; then
+	# copy this back
+	ifdh cp -D ${newDetsimFile} ${FULL_DETSIM_OUTDIR} && echo "COPYING DETSIM FILE $(basename ${newDetsimFile}) TO ${FULL_DETSIM_OUTDIR}..."
+	echo "DECLARING DETSIM ARTROOT FILE ${newDetsimFile}"
+	samweb -e ${SAM_EXPERIMENT} declare-file ${DETSIM_MD}
+	samweb -e ${SAM_EXPERIMENT} add-file-location $(basename ${newDetsimFile}) ${FULL_DETSIM_OUTDIR}
+	echo -e "samweb -e ${SAM_EXPERIMENT} add_file_location $(basename ${newDetsimFile}) ${FULL_DETSIM_OUTDIR}"
+	
+	echo -e "Creating definition ${DETSIM_DTAG}..."
+	samweb -e ${SAM_EXPERIMENT} create-definition ${DETSIM_DTAG} "Dataset.Tag ${DETSIM_DTAG}"
+    fi
+fi # copy back detsim?
 ###### RECO1 DECLARATION ######
 if samweb -e ${SAM_EXPERIMENT} validate-metadata ${RECO1_MD} ; then
     # copy this back
@@ -349,4 +386,4 @@ fi
 
 echo -e "Done"
 
-echo "metadata_postscripts.sh done"
+echo "$0 done"
